441505c230adb06fd492fe5c469ffdc9d94c24c1
Andre Gurgel <andreggurgelufrn@gmail.com>
2020-06-19 16:26:19 -0300

    Adicionando mensagens para o usuario e retirando importações sem uso.

--- algamoney-api/src/main/java/br/com/gurgel/algamoneyapi/model/Lancamento.java
+++ algamoney-api/src/main/java/br/com/gurgel/algamoneyapi/model/Lancamento.java
@@ -1,6 +1,7 @@
 package br.com.gurgel.algamoneyapi.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
@@ -14,25 +15,31 @@ public class Lancamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
 
+    @NotNull
     private String descricao;
 
+    @NotNull
     @Column(name = "data_vencimento")
     private LocalDate dataVencimento;
 
     @Column(name = "data_pagamento")
     private LocalDate dataPagemento;
 
+    @NotNull
     private BigDecimal valor;
 
     private String observacao;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private TipoLancamento tipo;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "codigo_categoria")
     private Categoria categoria;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "codigo_pessoa")
     private Pessoa pessoa;
