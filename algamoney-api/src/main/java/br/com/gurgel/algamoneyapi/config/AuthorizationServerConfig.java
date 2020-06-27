5c0479a2fd2c66acc326087520df22a1eeb528e9
Andre <andreggurgelufrn@gmail.com>
2020-06-27 13:58:27 -0300

    Configurando Jwt no projeto.

--- algamoney-api/src/main/java/br/com/gurgel/algamoneyapi/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/br/com/gurgel/algamoneyapi/config/AuthorizationServerConfig.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -34,11 +33,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore())
+                .accessTokenConverter(AccessTokenConverter())
                 .authenticationManager(authenticationManager);
     }
 
     @Bean
+    public JwtAccessTokenConverter AccessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("algaworks");
+        return accessTokenConverter;
+    }
+
+    @Bean
     public TokenStore tokenStore(){
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(AccessTokenConverter());
     }
 }
