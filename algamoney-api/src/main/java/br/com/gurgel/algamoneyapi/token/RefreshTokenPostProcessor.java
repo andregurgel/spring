12b4c4dab86fa8bdd248ebbf2dfa18b9f72e329c
Andre <andreggurgelufrn@gmail.com>
2020-06-27 15:52:57 -0300

    Movendo o refresh token para o cookie.

--- algamoney-api/src/main/java/br/com/gurgel/algamoneyapi/token/RefreshTokenPostProcessor.java
+++ algamoney-api/src/main/java/br/com/gurgel/algamoneyapi/token/RefreshTokenPostProcessor.java
@@ -9,12 +9,14 @@ import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@ControllerAdvice
 public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
     @Override
     public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
