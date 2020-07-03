12c92454bbcda53724bf457b4652b94c3ad118fc
Andre <andreggurgelufrn@gmail.com>
2020-07-03 15:23:31 -0300

    Reorganizando repositorio.

--- maker_example/src/main/java/com/example/maker_example/domain/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.maker_example.domain.repository;
-
-import com.example.maker_example.domain.models.Car;
-import com.github.gustavovitor.maker.repository.RepositoryMaker;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends RepositoryMaker<Car, Long> {
-}
